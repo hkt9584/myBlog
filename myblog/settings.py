22267249fc67f717339f9a7801d0826bdac7b054
hkt9584 <hkt9584@gmail.com>
2024-04-06 23:54:08 +0800

    更新資料庫為mysql

--- myblog/settings.py
+++ myblog/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "myblog",
+        "USER": "myblog",
+        "PASSWORD": "p@ssw0rd",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
