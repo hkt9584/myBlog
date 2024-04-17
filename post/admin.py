7592246e3cfa9a7dc3f3de246aa4866b2af1edbf
hkt9584 <hkt9584@gmail.com>
2024-04-17 23:03:46 +0800

    author & created_date

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'author', 'created_date')
+    search_fields = ('title', 'content', 'author', 'created_date')
+
+    admin.site.register(Post)
--- post/models.py
+++ post/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField('標題', max_length=20)
     content = models.CharField('內容', max_length=300)
+    author = models.CharField(max_length=20,default='SOME STRING')
+    created_date = models.DateTimeField(default=timezone.now)
