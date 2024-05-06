faed29101ddfad676b4d3e17862c1385e448017d
hkt9584 <hkt9584@gmail.com>
2024-05-06 22:10:47 +0800

    終於可以display了（post,PostAdmin)

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,4 @@
+from .models import (Post, PostAdmin) 
 from django.contrib import admin
-from .models import Post
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'author', 'created_date')
-    search_fields = ('title', 'content', 'author', 'created_date')
-
-    admin.site.register(Post)
+admin.site.register(Post, PostAdmin) 
--- post/models.py
+++ post/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.contrib import admin
 
 class Post(models.Model):
     title = models.CharField('標題', max_length=20)
     content = models.CharField('內容', max_length=300)
-    author = models.CharField(max_length=20,default='SOME STRING')
+    author = models.CharField(max_length=20,default='SOME STRING', blank=True)
     created_date = models.DateTimeField(default=timezone.now)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'author')
+    search_fields = ('title', 'content', 'author', 'created_date')
+     
