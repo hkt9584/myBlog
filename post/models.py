95962944dc1beacdcd4ce5b7849b10babd0d6806
hkt9584 <hkt9584@gmail.com>
2024-05-14 22:53:01 +0800

    創立模板。（真的有變！好酷！）

--- post/models.py
+++ post/models.py
@@ -4,10 +4,17 @@ from django.utils import timezone
 from django.contrib import admin
 
 class Post(models.Model):
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+
     title = models.CharField('標題', max_length=20)
     content = models.CharField('內容', max_length=300)
-    author = models.CharField(max_length=20,default='SOME STRING', blank=True)
+    author = models.CharField('作者', max_length=20,default='SOME STRING', blank=True)
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'author')
